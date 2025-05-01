53ab0a7862e373be77422cfd03d6c8dd73e75d81
Daniel Ferreira Lara <d2021003661@unifei.edu.br>
2025-05-01 17:13:43 +0200

    Refactor main function for improved sound task initialization and organization

--- src/main.c
+++ src/main.c
@@ -1,9 +1,25 @@
 #include "stm32f10x.h"
-
 #include "FreeRTOS.h"
+#include "task.h"
 
-int main() {
+#include "sound.h"
+
+TaskHandle_t handle_SoundTask;
+
+int main(void) {
+    // SoundConfig cfg = {SOUND_RESISTANCE, 0, 0};
+    // SoundConfig cfg = {SOUND_RESISTANCE, 5, 10}; 
+    SoundConfig cfg = {SOUND_E1, 0, 0};
+
+    SystemInit();
+    
+    vInit_soundTasks();
+    
+    setSoundState(cfg);
+
+    vTaskStartScheduler();
+    
     while(1) {
         // Do nothing
-    }
+    } 
 }
